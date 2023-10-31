9dae32b49fe755e36184d6b379be7581db1e98cf
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-10-31 08:10:52 -0500

    ok

--- analysis/analisVentas.py
+++ analysis/analisVentas.py
@@ -1,7 +1,34 @@
 import pandas as pd
 
-from helpers.crearCSV import crearCSV
+from helpers.crearVentasCSV import crearCSV
+from helpers.crearTablaHTML import crearTabla
 from data.ventas import ventas
 
-#1. Crear un CSV con los datos de las ventas
-crearCSV(ventas,'ventas.csv')
+#1 Crear un CSV con los datos de las ventas
+crearCSV(ventas, 'ventas.csv')
+
+#2 cargo la fuente de datos y con PANDAS creo un DATAFRAME
+ventasDataFrame=pd.read_csv('data/ventas.csv')
+crearTabla(ventasDataFrame, 'tablaventas')
+
+#3 explorar los datos
+'''examen1=ventasDataFrame.head()
+examen2=ventasDataFrame.tail()
+examen3=ventasDataFrame.head(20)
+examen4=ventasDataFrame.info()
+examen5=ventasDataFrame.describe()
+examen6=ventasDataFrame.tail(50)'''
+
+
+#4 filtrar y ordenar (limpiar)
+filtroUno=ventasDataFrame.query("(Costo>=290000) and (Costo<=300000)")
+totalventas=filtroUno[['Costo', 'Cliente']]
+
+#Generando html con resultados del filtro
+crearTabla(filtroUno, 'ventasBajoCosto')
+
+
+#5 modelar o aplicar modelos 
+
+
+#6 presentar y exportar los datos
--- data/empleados.py
+++ data/empleados.py
@@ -1,41 +1,17 @@
 import random
-empleado1={
-    "id":1,
-    "nombre":"Juan Jose",
-    "apellidos":"Gallego Mesa",
-    "salariomensual":15000000,
-    "deudas":True,
-    "retencionFuente":0.1,
-    "correo":"jjme@grupouribe.com",
-    "telefono":"3225962363",
-    "cargo":"Arquitecto"
-}
 
-empleado2={
-    "id":2,
-    "nombre":"Johan",
-    "apellidos":"Valderrama",
-    "salariomensual":450000,
-    "deudas":False,
-    "retencionFuente":0.0,
-    "correo":"jhv@grupouribe.com",
-    "telefono":"3208795623",
-    "cargo":"Jr"
-}
+empleados=[]
 
-# desarrollador junior 
-# desarrollador intermedio 
-# desarrollador avanzado  
-# arquitecto
-
-#SOLO si se ganan mas de 6 millones se aplica retencion en la fuente del 10%
-
-empleados = []
-for _ in range(50):
-    nombre = random.choice(['Andres', 'Ana', 'Isabel', 'Pablo'])
-    cargo = random.choice(['Gerente', 'Desarrollador', 'Diseñador', 'Analista'])
-    edad = random.randint(22, 60)
-    salario = random.randint(1160000, 18000000)
-    empleado = [nombre, cargo, edad, salario]
+for _ in range(2000):
+    
+    ide=random.randint(0,500000)
+    nombre=random.choice(['Andres', 'Ana', 'Isabel', 'Pablo', 'Juan', 'Kelly', 'Mauricio', 'Manuela', 'Veronica', 'Raul'])
+    apellidos=random.choice(['Montoya','Perez','Gonzalez', 'Peralta', 'Gomez', 'Urrego', 'Gutierrez' ])
+    salariomensual=random.randint(1000000, 5000000)
+    deudas=True
+    retencionfuente=0.1
+    correo=random.choice(['aaaa@hotmail.com', 'bbbb@hotmail.com', 'cccc@hotmail.com', 'dddd@hotmail.com'])
+    telefono=random.randint(4598760,5082345)
+    cargo=random.choice(['arquitecto', 'desarrollador junior', 'desarrollador senior'])
+    empleado = [ide, nombre, apellidos, salariomensual, deudas, retencionfuente, correo, telefono, cargo]
     empleados.append(empleado)
-print(empleados)
